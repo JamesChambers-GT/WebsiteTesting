cfd47afda9da9e7c602c67c96fe0bef06ef1c130
JamesChambers-GT <chambers.james.b@gmail.com>
2025-07-21 11:24:46 -0400

    pt4

--- server.js
+++ server.js
@@ -23,3 +23,7 @@ app.get('/p3', (req, res) => {
 app.get('/home', (req,res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
+
+app.get('/', (req,res) => {
+  res.sendFile(path.join(__dirname,'public', 'index.html'))
+})
