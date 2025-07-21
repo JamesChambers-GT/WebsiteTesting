bdebce8dbfd53921d280de8b2eb31c757f6e384e
JamesChambers-GT <chambers.james.b@gmail.com>
2025-07-21 10:58:56 -0400

    Subpage clean

--- server.js
+++ server.js
@@ -7,3 +7,19 @@ app.use(express.static('public'));
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+app.get('/page1', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'page1.html'));
+});
+
+app.get('/page2', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'page2.html'));
+});
+
+app.get('/page3', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'page3.html'));
+});
+
+app.get('/home', (req,res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
